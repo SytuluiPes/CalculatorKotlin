231fdb4edc9291a290b1b3d8153493f6e8ac5d0b
SytuluiPes <artemkovalev2014@gmail.com>
2023-10-19 22:00:45 +0300

    import fonts, resizes

--- app/src/main/java/com/example/calculatorisworking/MainActivity.kt
+++ app/src/main/java/com/example/calculatorisworking/MainActivity.kt
@@ -11,7 +11,7 @@ import kotlin.math.roundToInt
 import androidx.lifecycle.findViewTreeViewModelStoreOwner
 import com.google.android.material.button.MaterialButton
 import java.lang.Exception
-
+import kotlin.math.roundToLong
 
 
 object FunResult {
@@ -20,16 +20,21 @@ object FunResult {
     var answer: Double = 0.0
     var saved_value : Double? = null
 }
+
+enum class Buttons{
+    Plus, Minus, Division, Multiply, Equility,
+}
+
 object FunAction {
     var actionflag: Boolean = false
     var btn_name: String = "btn_quality"
     var prev_btn_name : String = ""
     var input_text : Boolean = false
 }
-// commit
 class MainActivity() : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //
         setContentView(R.layout.activity_main)
         val btn_0 : Button = findViewById(R.id.btn_0)
         btn_0.setOnClickListener {setText("0")}
@@ -72,13 +77,20 @@ class MainActivity() : AppCompatActivity() {
         delText()
     }
     fun setText(str: String) {
+        test("setText - START")
         val result_text: TextView = findViewById(R.id.result_text)
         if (result_text.text.toString() == "0" && str == "0") return Unit
         if (!FunAction.input_text && !FunAction.actionflag) {
             result_text.text = str
             FunAction.input_text = true
+            FunResult.number1 = result_text.text.toString().toDouble()
+            result_text.textSize = 80F
         }
-        else if (FunAction.input_text && !FunAction.actionflag) result_text.append(str)
+        else if (FunAction.input_text && !FunAction.actionflag) {
+            result_text.append(str)
+            FunResult.number1 = result_text.text.toString().toDouble()
+            result_text.textSize = 80F
+        }
         else if (!FunAction.input_text && FunAction.actionflag) {
             result_text.text = str
             FunAction.input_text = true
@@ -88,24 +100,40 @@ class MainActivity() : AppCompatActivity() {
             result_text.append(str)
             FunResult.number2 = result_text.text.toString().toDouble()
         }
-
+        resizeText()
         if (result_text.text.toString() != "0") {
             val btn_C : Button = findViewById(R.id.btn_C)
             btn_C.text = "C"
         }
+        test("setText - END")
     }
 
     fun setPercent(){
+        test("setPercent - START")
         val result_text: TextView = findViewById(R.id.result_text)
         var result_division_hundred : Double = result_text.text.toString().toDouble()
         result_division_hundred /= 100
-        result_text.text = result_division_hundred.toString()
+        result_division_hundred = (result_division_hundred * 10000000.0).roundToLong() / 10000000.0
+        if (result_division_hundred == result_division_hundred.toLong().toDouble())
+            result_text.text = result_division_hundred.toLong().toString()
+        else
+            result_text.text = result_division_hundred.toString()
+        if (!FunAction.actionflag)
+            FunResult.number1 = result_text.text.toString().toDouble()
+        else if (FunResult.number2 == null)
+            FunResult.number1 = result_text.text.toString().toDouble()
+        else
+            FunResult.number2 = result_text.text.toString().toDouble()
+        test("setPercent - END")
     }
 
     fun setChar(){
+        test("setChar - START")
         val result_text: TextView = findViewById(R.id.result_text)
         var result_text_setChar = ""
+        var textsize = 0f
         if (result_text.text.toString().indexOf("-") == -1){
+            result_text.textSize = textsize
             result_text_setChar = "-${result_text.text}"
             result_text.text = result_text_setChar
         }
@@ -113,27 +141,43 @@ class MainActivity() : AppCompatActivity() {
             result_text_setChar = result_text.text.toString().replace("-","")
             result_text.text = result_text_setChar
         }
+        resizeText()
+        if (!FunAction.actionflag)
+            FunResult.number1 = result_text.text.toString().toDouble()
+        else if (FunResult.number2 == null)
+            FunResult.number1 = result_text.text.toString().toDouble()
+        else
+            FunResult.number2 = result_text.text.toString().toDouble()
+        test("setChar - END")
     }
 
     fun setPoint(){
+        test("setPoint - START")
         val result_text: TextView = findViewById(R.id.result_text)
         var result_text_setPoint = ""
         if (result_text.text.toString().indexOf(".") == -1) {
             result_text_setPoint = "${result_text.text}."
             result_text.text = result_text_setPoint
         }
+        FunAction.input_text = true
+        if (!FunAction.actionflag)
+            FunResult.number1 = result_text.text.toString().toDouble()
+        else if (FunResult.number2 == null)
+            FunResult.number1 = result_text.text.toString().toDouble()
+        else
+            FunResult.number2 = result_text.text.toString().toDouble()
+        test("setPoint - END")
     }
     fun resultView() {
+        test("resultView - START")
         if (FunResult.number1 != null && (FunAction.btn_name != "btn_quality" ||
                     FunAction.prev_btn_name != "btn_quality")) {
             var btn: Button = findViewById(R.id.btn_equlity)
             val result_text: TextView = findViewById(R.id.result_text)
-            if (FunResult.saved_value == null && FunResult.number2 == null) {
+            if (FunResult.saved_value == null && FunResult.number2 == null)
                 FunResult.saved_value = FunResult.number1
-            }
-            if (FunResult.number2 == 0.0 && FunAction.btn_name == "btn_division"){
+            if ((FunResult.number1 == 0.0 || FunResult.number2 == 0.0) && FunAction.btn_name == "btn_division")
                 result_text.text = "0"
-            }
             else {
                 if (FunResult.number2 != null) {
                     when (if (FunAction.btn_name != "btn_quality") FunAction.btn_name
@@ -181,7 +225,7 @@ class MainActivity() : AppCompatActivity() {
                     i("", "FunResult.answer = ${FunResult.answer}")
                     result_text.text = FunResult.answer.toLong().toString()
                 } else {
-                    FunResult.answer = (FunResult.answer * 10000.0).toLong() / 10000.0
+                    FunResult.answer = (FunResult.answer * 10000000.0).toLong() / 10000000.0
                     result_text.text = FunResult.answer.toString()
                 }
             }
@@ -202,35 +246,31 @@ class MainActivity() : AppCompatActivity() {
                     btn = findViewById(R.id.btn_multiply)
                 }
             }
-            i("","""
-            |FunResult.number1 = ${FunResult.number1}
-            |FunResult.number2 = ${FunResult.number2}
-            |FunAction.actionflag = ${FunAction.actionflag}
-            |FunAction.btn_name = ${FunAction.btn_name}
-            |FunAction.prev_btn_name = ${FunAction.prev_btn_name}
-            |FunResult.answer = ${FunResult.answer}
-            |FunResult.saved_value = ${FunResult.saved_value}
-        """.trimMargin())
             btn.setBackgroundColor(Color.parseColor("#ffaf25"))
             btn.setTextColor(Color.parseColor("#ffffff"))
             FunAction.actionflag = false
             FunAction.input_text = false
+            if (FunResult.number2 != null) FunResult.saved_value = FunResult.number2
             FunResult.number2 = null
             FunResult.number1 = FunResult.answer
             if (FunAction.btn_name != "btn_quality") FunAction.prev_btn_name = FunAction.btn_name
             FunAction.btn_name = "btn_quality"
-
         }
+        resizeText()
+        test("resultView - END")
     }
 
     fun delText() {
+        test("delText - START")
         val result_text: TextView = findViewById(R.id.result_text)
         var btn : Button = findViewById(R.id.btn_equlity)
         val btn_C : Button = findViewById(R.id.btn_C)
         if (result_text.text.toString() != "0"){
             result_text.text = "0"
             FunResult.saved_value = null
+            FunAction.input_text = false
             btn_C.text = "AC"
+            result_text.textSize = 80F
         }
         else if (FunAction.actionflag == true){
             when (FunAction.btn_name){
@@ -251,13 +291,16 @@ class MainActivity() : AppCompatActivity() {
             btn.setTextColor(Color.parseColor("#ffffff"))
             FunAction.btn_name = "btn_equlity"
             FunAction.actionflag = false
-            FunAction.input_text = false
             FunResult.number1 = null
             FunResult.number2 = null
+            FunResult.answer = 0.0
+            FunAction.prev_btn_name = ""
+            FunResult.saved_value = null
         }
+        test("delText - END")
     }
     fun action(btn_name : String) {
-        val result_text: TextView = findViewById(R.id.result_text)
+        test("action - START")
         var btn : Button = findViewById(R.id.btn_equlity)
         if (FunAction.btn_name != btn_name){
             when(FunAction.btn_name){
@@ -277,9 +320,39 @@ class MainActivity() : AppCompatActivity() {
             btn.setBackgroundColor(Color.parseColor("#ffffff"))
             btn.setTextColor(Color.parseColor("#ffaf25"))
             FunAction.btn_name = btn_name
-            FunResult.number1 = result_text.text.toString().toDouble()
             FunAction.input_text = false
             FunAction.actionflag = true
         }
+        test("action - END")
+    }
+    private fun resizeText(){
+        val result_text : TextView = findViewById(R.id.result_text)
+        var textsize = result_text.textSize
+        val paint = result_text.paint
+        val textViewWidth = result_text.width
+        var textWidth = paint.measureText(result_text.text.toString())
+        i("", """
+            |textsize = $textsize
+            |textWidth = $textWidth
+            |textViewWidth = $textViewWidth
+        """.trimMargin())
+        while (textWidth >= textViewWidth) {
+            textsize -= 3
+            result_text.textSize = textsize
+            textWidth = paint.measureText(result_text.text.toString())
+        }
+    }
+    private fun test(text : String = "None"){
+        val result_text: TextView = findViewById(R.id.result_text)
+        i(text,"""
+            |FunResult.number1 = ${FunResult.number1}
+            |FunResult.number2 = ${FunResult.number2}
+            |FunResult.answer = ${FunResult.answer}
+            |FunResult.saved_value = ${FunResult.saved_value}
+            |result_text.text = ${result_text.text}
+        """.trimMargin())
     }
 }
+// Проблема в работе с LONG переменными
+// Проблема деление на 0, с использование строки "Ошибка"
+// Проблема с изменение размера окна вывода ответа
